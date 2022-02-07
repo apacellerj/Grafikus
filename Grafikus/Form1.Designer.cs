22375100864402caff97a9ff313e409da409e8cb
apacellerj <91457869+apacellerj@users.noreply.github.com>
2022-02-07 10:24:47 +0100

    Gomb

--- Grafikus/Form1.Designer.cs
+++ Grafikus/Form1.Designer.cs
@@ -29,13 +29,25 @@ namespace Grafikus
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(326, 267);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -44,6 +56,8 @@ namespace Grafikus
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Grafikus/Form1.cs
+++ Grafikus/Form1.cs
@@ -12,6 +12,7 @@ namespace Grafikus
 {
     public partial class Form1 : Form
     {
+        private int counter = 0;
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +21,13 @@ namespace Grafikus
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+            button1.Text = counter.ToString();
+            
         }
     }
 }
