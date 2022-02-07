b9cd6c675600a273afa49f763bb437a5f186fafc
apacellerj <91457869+apacellerj@users.noreply.github.com>
2022-02-07 13:33:02 +0100

    Nagyjából kész

--- Grafikus/Form1.cs
+++ Grafikus/Form1.cs
@@ -13,6 +13,7 @@ namespace Grafikus
     public partial class Form1 : Form
     {
         private int counter = 0;
+        private object textBox1;
         public Form1()
         {
             InitializeComponent();
@@ -26,9 +27,17 @@ namespace Grafikus
         private void button1_Click_1(object sender, EventArgs e)
         {
             if (textBox1.Text.Lenght < textBox1.Text.Lenght > 4)
-            MessageBox.Show("Legalább négy karakter beütésével működik!"){
+            {
+                MessageBox.Show("Legalább négy karakter beütésével működik!")
             }
             int birhtyear = 0;
+            bool retVal = int.TryParse(textBox1.Text, out birhtyear);
+            if (!retVal)
+            {
+                MessageBox.Show("Hibás adat!");
+            }
+            DateTime currentDAteTime = DateTime.Now;
+            label12.Text = $"{currentDAteTimeDateTime.Year - birhtyear} éves vagy";
         }
     }
 }
